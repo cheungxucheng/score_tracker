ae896dd4867b106b3dac8ec4bfb187be3d8aeb1d
cheungxucheng <ryanxzhang2.0@gmail.com>
2026-08-13 00:39:17 -0700

    commented out server logic and switched to a tappable score display instead of increment and decrement buttons and added undo button

--- ScoreTrackerApp.swift
+++ ScoreTrackerApp.swift
@@ -41,10 +41,10 @@ struct BadmintonView: View {
 
     var body: some View {
         TabView {
-            ServeView(
-                isDoubles: true,
-                game: game
-            )
+            // ServeView(
+            //     isDoubles: true,
+            //     game: game
+            // )
 
             ScoreView(
                 game: $game
@@ -81,11 +81,11 @@ struct Player {
 struct GameState {
     private(set) var scoreA = 0
     private(set) var scoreB = 0
-    private(set) var servingTeam: Team = .teamA
+    // private(set) var servingTeam: Team = .teamA
     private(set) var gameNum = 1
     // a collection(arr?) of completed game score strings
     private(set) var completedGames: [String] = []
-    private(set) var serverNum = 0
+    // private(set) var serverNum = 0
     private(set) var gamesWonA = 0;
     private(set) var gamesWonB = 0;
     /*
@@ -94,12 +94,12 @@ struct GameState {
     2 : B1
     3 : B2
     */
-    private(set) var players = [
-        Player(team: .teamA, side: .right),
-        Player(team: .teamA, side: .left),
-        Player(team: .teamB, side: .right),
-        Player(team: .teamB, side: .left)
-    ]
+    // private(set) var players = [
+    //     Player(team: .teamA, side: .right),
+    //     Player(team: .teamA, side: .left),
+    //     Player(team: .teamB, side: .right),
+    //     Player(team: .teamB, side: .left)
+    // ]
 
     var isGameOver: Bool {
         let leadingScore = max(scoreA, scoreB)
@@ -114,8 +114,9 @@ struct GameState {
     }
 
     mutating func pointWon(by winningTeam: Team) {
+        guard !isMatchOver else { return }
         incrementScore(for: winningTeam)
-        updateService(afterPointWonBy: winningTeam)
+        // updateService(afterPointWonBy: winningTeam)
 
         if isGameOver {
             finishGame()
@@ -132,56 +133,56 @@ struct GameState {
         }
     }
 
-    private mutating func updateService(
-        afterPointWonBy winningTeam: Team
-    ) {
-        if winningTeam != servingTeam {
-            servingTeam = winningTeam
-            selectServerUsingScoreParity(for: winningTeam)
-        } else {
-            switchPlayerSides(for: winningTeam)
-        }
-    }
+    // private mutating func updateService(
+    //     afterPointWonBy winningTeam: Team
+    // ) {
+    //     if winningTeam != servingTeam {
+    //         servingTeam = winningTeam
+    //         selectServerUsingScoreParity(for: winningTeam)
+    //     } else {
+    //         switchPlayerSides(for: winningTeam)
+    //     }
+    // }
 
-    private mutating func selectServerUsingScoreParity(
-        for team: Team
-    ) {
-        // the idea is is that we select the current server from the winning team
-        // based on the score parity. we can have an index into the player arr 
-        // represent who is currently serving.
+    // private mutating func selectServerUsingScoreParity(
+    //     for team: Team
+    // ) {
+    //     // the idea is is that we select the current server from the winning team
+    //     // based on the score parity. we can have an index into the player arr 
+    //     // represent who is currently serving.
 
-        // issue being, how do we tell what the score is based on the team?
-        // score is currently being tracked as two ints, scoreA and scoreB
-        // but just because its named score
+    //     // issue being, how do we tell what the score is based on the team?
+    //     // score is currently being tracked as two ints, scoreA and scoreB
+    //     // but just because its named score
 
-        // could do: if team = .teamA, check scoreA, vice versa
+    //     // could do: if team = .teamA, check scoreA, vice versa
 
-        // if serving team is teamA then if the score is even then if 
-        let score = team == .teamA ? scoreA : scoreB
-        let player1 = team == .teamA ? 0 : 2
-        let player2 = player1 + 1
+    //     // if serving team is teamA then if the score is even then if 
+    //     let score = team == .teamA ? scoreA : scoreB
+    //     let player1 = team == .teamA ? 0 : 2
+    //     let player2 = player1 + 1
 
-        let desiredSide: Parity =
-            score % 2 == 0 ? .right : .left 
+    //     let desiredSide: Parity =
+    //         score % 2 == 0 ? .right : .left 
         
-        serverNum = players[player1].side == desiredSide ? player1 : player2
-    }
+    //     serverNum = players[player1].side == desiredSide ? player1 : player2
+    // }
 
-    private mutating func switchPlayerSides(
-        for team: Team
-    ) {
-        // Add doubles position-switching logic here.
-        if (team == .teamA) {
-            let temp: Parity = players[0].side
-            players[0].side = players[1].side
-            players[1].side = temp
-        }
-        else {
-            let temp: Parity = players[2].side
-            players[2].side = players[3].side
-            players[3].side = temp
-        }
-    }
+    // private mutating func switchPlayerSides(
+    //     for team: Team
+    // ) {
+    //     // Add doubles position-switching logic here.
+    //     if (team == .teamA) {
+    //         let temp: Parity = players[0].side
+    //         players[0].side = players[1].side
+    //         players[1].side = temp
+    //     }
+    //     else {
+    //         let temp: Parity = players[2].side
+    //         players[2].side = players[3].side
+    //         players[3].side = temp
+    //     }
+    // }
 
     private mutating func finishGame() {
         completedGames.append("\(scoreA)-\(scoreB)")
@@ -202,9 +203,11 @@ struct GameState {
 
     mutating func resetMatch() { 
         completedGames = []
-        gameNum = 1;
+        gameNum = 1
         gamesWonA = 0
         gamesWonB = 0
+        scoreA = 0
+        scoreB = 0
     }
 
     mutating func removePoint(from team: Team) {
@@ -218,108 +221,108 @@ struct GameState {
 }
 }
 
-struct ServeView: View {
-    let isDoubles: Bool
-    let game: GameState
+// struct ServeView: View {
+//     let isDoubles: Bool
+//     let game: GameState
 
-    var body: some View {
-        GeometryReader { geo in
-            let w = geo.size.width
-            let h = geo.size.height
+//     var body: some View {
+//         GeometryReader { geo in
+//             let w = geo.size.width
+//             let h = geo.size.height
 
-            ZStack {
-                courtLines(width: w, height: h)
+//             ZStack {
+//                 courtLines(width: w, height: h)
 
-                opponentPlayers(width: w, height: h)
+//                 opponentPlayers(width: w, height: h)
 
-                userPlayers(width: w, height: h)
+//                 userPlayers(width: w, height: h)
 
-                servingIndicator(width: w, height: h)
-            }
-            .background(.green)
-        }
-    }
-    @ViewBuilder
-    private func userPlayers(width: CGFloat, height: CGFloat) -> some View {
-        Circle()
-            .fill(.blue)
-            .frame(width: 0.2 * height, height: 0.2 * height)
-            .position(x: 0.25 * width, y: 0.85 * height)
+//                 servingIndicator(width: w, height: h)
+//             }
+//             .background(.green)
+//         }
+//     }
+//     @ViewBuilder
+//     private func userPlayers(width: CGFloat, height: CGFloat) -> some View {
+//         Circle()
+//             .fill(.blue)
+//             .frame(width: 0.2 * height, height: 0.2 * height)
+//             .position(x: 0.25 * width, y: 0.85 * height)
 
-        if isDoubles {
-            Circle()
-                .stroke(.blue, lineWidth: 7)
-                .frame(width: 0.2 * height, height: 0.2 * height)
-                .position(x: 0.75 * width, y: 0.85 * height)
-        }
-    }
+//         if isDoubles {
+//             Circle()
+//                 .stroke(.blue, lineWidth: 7)
+//                 .frame(width: 0.2 * height, height: 0.2 * height)
+//                 .position(x: 0.75 * width, y: 0.85 * height)
+//         }
+//     }
 
-    @ViewBuilder
-    private func opponentPlayers(width: CGFloat, height: CGFloat) -> some View {
-        RoundedRectangle(cornerRadius: 4)
-            .fill(.red)
-            .frame(width: 0.2 * height, height: 0.2 * height)
-            .position(x: 0.25 * width, y: 0.15 * height)
+//     @ViewBuilder
+//     private func opponentPlayers(width: CGFloat, height: CGFloat) -> some View {
+//         RoundedRectangle(cornerRadius: 4)
+//             .fill(.red)
+//             .frame(width: 0.2 * height, height: 0.2 * height)
+//             .position(x: 0.25 * width, y: 0.15 * height)
 
-        if isDoubles {
-            RoundedRectangle(cornerRadius: 4)
-                .stroke(.red, lineWidth: 7)
-                .frame(width: 0.2 * height, height: 0.2 * height)
-                .position(x: 0.75 * width, y: 0.15 * height)
-        }
-    }
+//         if isDoubles {
+//             RoundedRectangle(cornerRadius: 4)
+//                 .stroke(.red, lineWidth: 7)
+//                 .frame(width: 0.2 * height, height: 0.2 * height)
+//                 .position(x: 0.75 * width, y: 0.15 * height)
+//         }
+//     }
 
-    @ViewBuilder
-    private func servingIndicator(
-        width: CGFloat,
-        height: CGFloat
-    ) -> some View {
-        if game.servingTeam == .teamA {
-            Circle()
-                .stroke(.yellow, lineWidth: 3)
-                .frame(width: 40, height: 40)
-                .position(
-                    x: width * 0.25,
-                    y: height * 0.85
-                )
-        } else {
-            RoundedRectangle(cornerRadius: 4)
-                .stroke(.yellow, lineWidth: 3)
-                .frame(width: 40, height: 40)
-                .position(
-                    x: width * 0.25,
-                    y: height * 0.15
-                )
-        }
-    }
-}
+//     @ViewBuilder
+//     private func servingIndicator(
+//         width: CGFloat,
+//         height: CGFloat
+//     ) -> some View {
+//         if game.servingTeam == .teamA {
+//             Circle()
+//                 .stroke(.yellow, lineWidth: 3)
+//                 .frame(width: 40, height: 40)
+//                 .position(
+//                     x: width * 0.25,
+//                     y: height * 0.85
+//                 )
+//         } else {
+//             RoundedRectangle(cornerRadius: 4)
+//                 .stroke(.yellow, lineWidth: 3)
+//                 .frame(width: 40, height: 40)
+//                 .position(
+//                     x: width * 0.25,
+//                     y: height * 0.15
+//                 )
+//         }
+//     }
+// }
 
 // Viewbuilder technically not needed for court lines, since it only returns one path?
-@ViewBuilder
-private func courtLines(
-    width: CGFloat,
-    height: CGFloat
-) -> some View {
-    Path { path in
-        // Opponent short service line
-        path.move(to: CGPoint(x: 0, y: 0.3 * height))
-        path.addLine(to: CGPoint(x: width, y: 0.3 * height))
+// @ViewBuilder
+// private func courtLines(
+//     width: CGFloat,
+//     height: CGFloat
+// ) -> some View {
+//     Path { path in
+//         // Opponent short service line
+//         path.move(to: CGPoint(x: 0, y: 0.3 * height))
+//         path.addLine(to: CGPoint(x: width, y: 0.3 * height))
 
-        // Opponent center line
-        path.move(to: CGPoint(x: 0.5 * width, y: 0))
-        path.addLine(to: CGPoint(x: 0.5 * width, y: 0.3 * height))
+//         // Opponent center line
+//         path.move(to: CGPoint(x: 0.5 * width, y: 0))
+//         path.addLine(to: CGPoint(x: 0.5 * width, y: 0.3 * height))
 
-        // User short service line
-        path.move(to: CGPoint(x: 0, y: 0.7 * height))
-        path.addLine(to: CGPoint(x: width, y: 0.7 * height))
+//         // User short service line
+//         path.move(to: CGPoint(x: 0, y: 0.7 * height))
+//         path.addLine(to: CGPoint(x: width, y: 0.7 * height))
 
-        // User center line
-        path.move(to: CGPoint(x: 0.5 * width, y: height))
-        path.addLine(to: CGPoint(x: 0.5 * width, y: 0.7 * height))
-    }
-    .stroke(.white, lineWidth: 7)
+//         // User center line
+//         path.move(to: CGPoint(x: 0.5 * width, y: height))
+//         path.addLine(to: CGPoint(x: 0.5 * width, y: 0.7 * height))
+//     }
+//     .stroke(.white, lineWidth: 7)
     
-}
+// }
 
 
 struct ScoreView: View {
